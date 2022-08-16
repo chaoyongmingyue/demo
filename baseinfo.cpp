53aec5356b239c711eb21e3f35ead8c2ae358d69
chaoyongmingyue <chaoyongmingyue@163.com>
2022-08-16 10:28:01 +0800

    'department'

--- baseinfo.cpp
+++ baseinfo.cpp
@@ -2,5 +2,26 @@
 
 BaseInfo::BaseInfo(QWidget *parent) : QWidget(parent)
 {
+    /*左侧*/
+    UserNameLabel = new QLabel(tr("用户名："));
+    UserNameLineEdit = new QLineEdit;
+    NameLabel = new QLabel(tr("姓名："));
+    NameLineEdit = new QLineEdit;
+    SexLabel = new QLabel(tr("性别："));
+    SexComboBox = new QComboBox;
+    SexComboBox->addItem(tr("女："));
+    SexComboBox->addItem(tr("男："));
+    DepartmentLabel = new QLabel(tr("部门："));
+    DepartmentTextEdit = new QTextEdit;
+    AgeLabel = new QLabel(tr("年龄："));
+    AgeLineEdit = new QLineEdit;
+    OtherLabel = new QLabel(tr("备注："));
+    OtherLabel->setFrameStyle(QFrame::Panel|QFrame::Sunken);
 
+    LeftLayout = new QGridLayout();
+    LeftLayout->addWidget(UserNameLabel,1,0,0);
+    LeftLayout->addWidget(UserNameLineEdit,0,1);
+    LeftLayout->addWidget(NameLabel,1,1);
+
+    /*右侧*/
 }
